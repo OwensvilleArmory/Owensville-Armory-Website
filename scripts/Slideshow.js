dbdfbbe1c638053bfdc52112588d266b66feb7a0
OwensvilleArmory <owensvillearmory@gmail.com>
2025-12-16 13:31:49 -0600

    Update Slideshow.js
    Added
    10-22_Christmas_Bundle.png
    
    Removed
    MPA_Defender.png
    Diamondback_DBX57.png
    Ruger_10-22_Sporter.png

--- scripts/Slideshow.js
+++ scripts/Slideshow.js
@@ -2,11 +2,13 @@ const SlideQuery = document.querySelector('.Image');
         
 const ImgArray = [
   "https://owensvillearmory.com/Featured_Products/Christmas_Savings.png",
+  //"https://owensvillearmory.com/Featured_Products/Atrius_FRT.png",
+  "https://owensvillearmory.com/Featured_Products/10-22_Christmas_Bundle.png",
   "https://owensvillearmory.com/Featured_Products/ATI_3B.png",
   "https://owensvillearmory.com/Featured_Products/RXM_FLAG.png",
-  "https://owensvillearmory.com/Featured_Products/MPA_Defender.png",
-  "https://owensvillearmory.com/Featured_Products/Diamondback_DBX57.png",
-  "https://owensvillearmory.com/Featured_Products/Ruger_10-22_Sporter.png",
+  //"https://owensvillearmory.com/Featured_Products/MPA_Defender.png",
+  //"https://owensvillearmory.com/Featured_Products/Diamondback_DBX57.png",
+  //"https://owensvillearmory.com/Featured_Products/Ruger_10-22_Sporter.png",
   //"https://owensvillearmory.com/Featured_Products/MAC_5.png",
   //"https://owensvillearmory.com/Featured_Products/Rossi_R92_1776.png",
   //"https://owensvillearmory.com/Featured_Products/LongGuns.png",
