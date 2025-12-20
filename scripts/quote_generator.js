34cbb86c3adb0900335528cb677ba76ff2d9a142
OwensvilleArmory <owensvillearmory@gmail.com>
2025-12-20 15:42:10 -0600

    Update quote_generator.js
    Updated to V2

--- scripts/quote_generator.js
+++ scripts/quote_generator.js
@@ -1,10 +1,20 @@
+//vars
 dropzone = document.getElementById("dropzone");
+displayimage = document.getElementById("imagePreview");
+HTMLDate = document.getElementById("Date");
+manufacturer = document.getElementsByClassName("Brand");//[0]//.value
+model = document.getElementsByClassName("Model");
+caliber = document.getElementsByClassName("Caliber");
+upc = document.getElementsByClassName("upc");
+price = document.getElementsByClassName("Price");
+download = document.getElementsByClassName("Download");
+days = 30;
+
+//create event listeners
 dropzone.addEventListener("drop", dropHandler);
 document.addEventListener("dragover", dragOverHandler);
-displayimage = document.getElementById("imagePreview");
 
-HTMLDate = document.getElementById("Date");
-
+//Handles loading the image
 function dropHandler(e) {
   e.preventDefault();
 
@@ -14,15 +24,10 @@ function dropHandler(e) {
   });
 
   file0.src = URL.createObjectURL(file0);
-  //console.log(file0.src);
 
   displayImage = document.createElement("img");
   displayImage.src = file0.src;
   displayImage.style.width = "100%";
-  //displayImage.style.width = "250%";
-  //height: 70px;
-  //width: 350px;
-  //padding: 10px;
     
   dropzone.appendChild(displayImage); 
   dropzone.style.border = "none";
@@ -31,29 +36,64 @@ function dropHandler(e) {
 function dragOverHandler(e) {
   e.preventDefault();
 }
-getDate();
-function getDate() {
-  days = 30;
 
-  currentDate = new Date();
-  newDate = addDays(currentDate, days);
+//Gets the valid until date
+currentDate = new Date();
+newDate = addDays(currentDate, days);
 
-  function addDays(date, days) {
-      newDate = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
-      if (newDate.getDay() == 1) {  //add an extra day if final day lands on a monday
-        console.log("Monday");
-        newDate = new Date(date.getTime() + (days+1) * 24 * 60 * 60 * 1000);
-      }
-      return newDate;
+function addDays(date, days) {
+  newDate = new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+  if (newDate.getDay() == 1) {  //add an extra day if final day lands on a monday
+    newDate = new Date(date.getTime() + (days+1) * 24 * 60 * 60 * 1000);
   }
-
-  console.log("New Date:", newDate.toDateString());
-
-  console.log(HTMLDate.innerText);
   HTMLDate.innerText = newDate.toDateString();
-  
+  return newDate;
 }
 
+//print on finish
 function printForm() {
-print();
+  randomize(8);
+  document.getElementById("upc").style.display="none";
+  price[0].value = "$"+price[0].value;
+  print();
+  getInformation();
+  createForm();
 };
+
+//creates a randomized string security code
+function randomize(l) {
+  const charset = "abcdefghijklmnopqrstuvwxyz01234567890123456789";
+  let result = "";
+  for (let i = 0; i < l; i++) {
+    result += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  document.getElementsByClassName("Code")[0].innerText=result;
+  idString = result;
+}
+
+//formats the file
+function getInformation() { 
+  fileInformation = [
+  manufacturer[0].value,
+  model[0].value,
+  caliber[0].value,
+  upc[0].value,
+  price[0].value,
+  "Valid Until: "+newDate.toDateString()
+]
+}
+
+//creates the file
+function createForm() {
+  let newFile = new File([fileInformation], idString+".txt");
+  
+  reader = new FileReader();
+
+  reader.onload = function(ev){
+    download[0].innerText = "Download: "+newFile.name;
+    download[0].href = URL.createObjectURL(newFile);
+    download[0].download = newFile.name;
+    download[0].click();
+  }
+reader.readAsText(newFile);
+}
