79cb75cac9cab5d7d60f7a3ec100d94002cf5b05
OwensvilleArmory <owensvillearmory@gmail.com>
2026-01-22 13:14:52 -0600

    Update Slideshow.js

--- scripts/Slideshow.js
+++ scripts/Slideshow.js
@@ -2,13 +2,13 @@ const SlideQuery = document.querySelector('.Image');
         
 const ImgArray = [
   "https://owensvillearmory.com/Featured_Products/Atrius Development Group FRS.png",
+  "https://owensvillearmory.com/Featured_Products/Taurus_1911.png",
   "https://owensvillearmory.com/Featured_Products/ATI_3B.png",
   "https://owensvillearmory.com/Featured_Products/RXM_FLAG.png",
-  //"https://owensvillearmory.com/Featured_Products/MPA_Defender.png",
+  "https://owensvillearmory.com/Featured_Products/MPA_Defender.png",
   //"https://owensvillearmory.com/Featured_Products/Diamondback_DBX57.png",
   //"https://owensvillearmory.com/Featured_Products/Ruger_10-22_Sporter.png",
   //"https://owensvillearmory.com/Featured_Products/MAC_5.png",
-  //"https://owensvillearmory.com/Featured_Products/Rossi_R92_1776.png",
   //"https://owensvillearmory.com/Featured_Products/LongGuns.png",
   "https://owensvillearmory.com/Featured_Products/Shark_Coast_Trump_Rifle.png",
   //"https://owensvillearmory.com/Featured_Products/Howa_M1500_Stakler_CF.png",
